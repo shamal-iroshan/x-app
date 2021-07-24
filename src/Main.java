694a852a38a59997c1c4e224f6c332d4e5d5b8e3
shamal-iroshan <shamaliroshan34@gmail.com>
2021-07-24 12:24:41 +0530

    change intents

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@
  * @since 7/24/21
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello X-App");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello X-App");
+  }
 }
